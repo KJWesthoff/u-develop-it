ad26400564f6847e75c674e70fd3a30328fffb58
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-12-04 22:15:21 -0800

    cRuD

--- server.js
+++ server.js
@@ -3,9 +3,93 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const sqlite3 = require('sqlite3').verbose();
+
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Db connect
+const db = new sqlite3.Database('./db/election.db', err => {
+    if(err) {
+        return console.error(err.message);
+    }
+    console.log("connected to the election database")
+})
+
+
+// Get all candidates
+app.get('/api/candidates', (req,res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+
+        res.json({
+            message:"success",
+            data:rows
+        });
+    });
+});
+
+// Api end point for single candidate
+app.get('/api/candidate/:id', (req,res) =>{
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql,params, (err,row) => {
+        if(err){
+            res.status(400).json({error: error.message});
+            return;
+        }   
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+
+
+// Api endpoint to Delete a db entry
+app.delete('/api/candidate/:id', (req,res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.run(sql,params,function(err, result) {
+        if(err){
+           
+            res.status(400).json({error:res.message});
+            return;
+        }
+
+        res.json({
+            message: 'succsessfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+
+
+
+// // Create an entry 
+// const sql = `INSERT INTO candidates 
+//     (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Fairbanks', 1];
+
+// db.run(sql, params, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID)
+// });
+
 
 
 // deafult response catch all
@@ -14,7 +98,9 @@ app.use((req,res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// start server after db connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
-
